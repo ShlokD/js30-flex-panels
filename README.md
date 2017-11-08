6bc0b62ffbc3a493655528af2204466c460c96f3
ShlokD <shlokpdesai@gmail.com>
2017-11-08 00:01:34 -0800

    Init

---
+++ README.md
@@ -0,0 +1,5 @@
+### Flexbox Panels
+
+Flexbox panels in CSS and Vanilla JS
+
+Images from Pixabay
---
+++ js/panels.js
@@ -0,0 +1,9 @@
+const onPanelClick = (ev) => {
+  ev.target.classList.toggle('open');
+  ev.target.classList.toggle('open-active');
+}
+
+const panels = document.querySelectorAll('.panel');
+
+
+panels.forEach(panel  => panel.addEventListener('click', onPanelClick));
